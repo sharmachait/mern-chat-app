a755a55d92f8f61fde0f4653f4888f3b960bb862
29!@#vansthaliA <chait8126@gmail.com>
2024-03-17 19:13:37 +0530

    yayyyyyyy

---
+++ Backend/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const authRouter = require('./routers/Auth');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+async function StartUp() {
+  try {
+    await mongoose.connect(process.env.MongoUrl);
+    const app = express();
+
+    app.use(bodyParser.json());
+    app.use('/auth', authRouter);
+
+    app.listen(process.env.PORT, console.log(`listening on ${process.env.PORT}`));
+  } catch (e) {
+    console.log("error " + e)
+  }
+}
+
+StartUp();
